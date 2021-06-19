2adcc19bb2d9f3df471895e283bb012b4135c6db
ProjektOpensource <69577313+ProjektOpensource@users.noreply.github.com>
2021-06-19 09:38:49 +0200

    Add files via upload

---
+++ Achtung_Virus.py
@@ -0,0 +1,68 @@
+import random
+import os
+
+#print("Do you like me?")
+#print("Y/N")
+
+print("Do you really wnat to run this Virus?")
+print("I will not be held responsible for any damage caused by my virus!!!")
+print("Y/N")
+
+Input_1 = input()
+
+#if ("N") == (Input_1):
+    #while True:
+        #bad = random.choice(["Fuck you!!!!", "You are stupid!!!", "I hate you!!!", '''import random
+
+        #print("Do you like me?")
+        #print("Yes/No")
+
+        #Input_1 = input()
+
+        #if ("No") == (Input_1):
+            #while True:
+                #bad = random.choice(["Fuck you!!!!", "You are stupid!!!", "I hate you!!!"])
+                #print(bad)
+
+        #if ("Yes") == (Input_1):
+            #while True:
+                #good = random.choice(["I like you.", "You are smart.", "Good day."])
+                #print(good)'''])
+        #print(bad)
+        #os.system('cmd /k "cd C:\Program Files\Windows Media Player & start wmplayer.exe"')
+
+if ("Y") == (Input_1):
+    while True:
+        good = random.choice(["I like you.", "You are smart.", "Good day.", '''import random
+
+        print("Do you like me?")
+        print("Yes/No")
+
+        Input_1 = input()
+
+        if ("No") == (Input_1):
+            while True:
+                bad = random.choice(["Fuck you!!!!", "You are stupid!!!", "I hate you!!!"])
+                print(bad)
+
+        if ("Yes") == (Input_1):
+            while True:
+                good = random.choice(["I like you.", "You are smart.", "Good day."])
+                print(good)'''])
+        print(good)
+        os.system('cmd /k "cd C:\Program Files\Windows Media Player & start wmplayer.exe"')
+
+elif ("N") == (Input_1):
+    print("Good choice.")
+
+#elif ("AGB") == (Input_1):
+    #print('''AGB's:
+
+#By open the ".py File" you agree with my agb's!!!:
+
+#I will not be held responsible for any damage caused by my virus!!!
+#Don't crash any Computers that you not allowed to!!!
+#You are by yourself responsible for everything you do with this Virus!!!''')
+
+else:
+    print("This anwer is not defined!!!")
